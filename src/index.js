e7e6e9603739824064ee7c9af0b5f5f86f1cb781
Bruno Germano <bruno.germano@gmail.com>
2017-02-24 22:29:06 +0000

    move firebase initialize to react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyBtsJfugUQIO6hxK5TRLknHbDjHoWG90to",
+  authDomain: "yt-posters.firebaseapp.com",
+  databaseURL: "https://yt-posters.firebaseio.com",
+  storageBucket: "yt-posters.appspot.com",
+  messagingSenderId: "340211926316"
+};
+
+Firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
